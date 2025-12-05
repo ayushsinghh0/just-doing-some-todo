ec4f83257a5dcfa5a7993f08f59689b6522c48a4
ayushsinghh0 <ayushraj4820@gmail.com>
2025-12-05 08:30:52 +0530

    learning new mongo db for authentication

---
+++ db.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongooose");
+const ObjectId=mongoose.ObjectId;
+
+const Schema=mongoose.Schema;
+const User=new Schema({
+    email: String,
+    password: String,
+    name:String
+})
+
+const Todo=new Schema({
+    title:String,
+    done:Boolean,
+    userId: ObjectId
+})
+
+const UserModel=mongoose.model('users',User);
+const TodoModel =mongoose.model('todos',Todo);
+
+module.exports={
+    UserModel:UserModel,
+    TodoModel:TodoModel
+}
